202ccbd4ce2f74cbcb4079cbdb31d21bbcdf4960
Fred Wong <fredwong.it@gmail.com>
2018-02-06 17:05:12 -0500

    Node.js Server Setup

---
+++ server/config.js
@@ -0,0 +1,6 @@
+// server/config.js
+module.exports = {
+    AUTH0_DOMAIN: 'fredwong-it.auth0.com', // e.g., kmaida.auth0.com
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://admin:admin@ds125198.mlab.com:25198/mean'
+  };
