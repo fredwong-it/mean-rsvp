95578e5a03283dce66bcdbcfdb604d00ee294c79
Fred Wong <fredwong.it@gmail.com>
2018-02-11 18:16:08 -0500

    feat: Angular: Finish RSVP Component Logic

--- src/app/pages/event/rsvp/rsvp.component.ts
+++ src/app/pages/event/rsvp/rsvp.component.ts
@@ -38,6 +38,7 @@ export class RsvpComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.footerTense = !this.eventPast ? 'plan to attend this event.' : 'attended this event.';
     this._getRSVPs();
+    this.toggleEditForm(false);
   }
 
   private _getRSVPs() {
@@ -66,23 +67,43 @@ export class RsvpComponent implements OnInit, OnDestroy {
 
   toggleEditForm(setVal?: boolean) {
     this.showEditForm = setVal !== undefined ? setVal : !this.showEditForm;
-    this.editBtnText = this.showEditForm ? 'Cancel Edit' : 'Edit RSVP';
+    this.editBtnText = this.showEditForm ? 'Cancel Edit' : 'Edit My RSVP';
   }
 
-  private _updateRsvpState() {
+  private _updateRsvpState(changed?: boolean) {
     // @TODO: We will add more functionality here later
-    this._setUserRsvpGetAttending();
+        // If RSVP matching user ID is already
+    // in RSVP array, set as initial RSVP
+    const _initialUserRsvp = this.rsvps.filter( rsvp => {
+      return rsvp.userId === this.auth.userProfile.sub;
+    })[0];
+
+    // If user has not RSVPed before and has made
+    // a change, push new RSVP to local RSVPs store
+    if (!_initialUserRsvp && this.userRsvp && changed) {
+      this.rsvps.push(this.userRsvp);
+    }
+
+    this._setUserRsvpGetAttending(changed);
   }
 
-  private _setUserRsvpGetAttending() {
+  private _setUserRsvpGetAttending(changed?: boolean) {
     // Iterate over RSVPs to get/set user's RSVP
     // and get total number of attending guests
     let guests = 0;
     const rsvpArr = this.rsvps.map(rsvp => {
       // If user has an existing RSVP
       if (rsvp.userId === this.auth.userProfile.sub) {
-        this.userRsvp = rsvp;
+        if (changed) {
+          // If user edited their RSVP, set with updated data
+          rsvp = this.userRsvp;
+        } else {
+          // If no changes were made, set userRsvp property
+          // (This applies on ngOnInit)
+          this.userRsvp = rsvp;
+        }
       }
+
       // Count total number of attendees
       // + additional guests
       if (rsvp.attending) {
@@ -100,9 +121,7 @@ export class RsvpComponent implements OnInit, OnDestroy {
   onSubmitRsvp(e) {
     if (e.rsvp) {
       this.userRsvp = e.rsvp;
-      // @TODO: update _updateRsvpState() method
-      // to support 'changed' parameter:
-      // this._updateRsvpState(true);
+      this._updateRsvpState(true);
       this.toggleEditForm(false);
     }
   }
